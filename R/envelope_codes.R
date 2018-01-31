0c90ec29f21a9d9419a84ec53abc9a9d0534e661
richardjtelford <richard.telford@bio.uib.no>
2018-01-31 19:58:40 +0100

    initial commit

---
+++ R/envelope_codes.R
@@ -0,0 +1,10 @@
+#generate_valid filenames
+library(tidyverse)
+library(R.utils)
+
+all_codes <- crossing(A = LETTERS, B = LETTERS, C = LETTERS) %>% 
+  mutate(code = paste0(A, B, C), 
+         hash = (1L:n()) %% 10000L,
+         hash = withSeed(sample(hash), seed = 1),
+         hash = formatC(hash, width = 4, format = "d", flag = "0"),
+         hashcode = paste0(code, hash))
