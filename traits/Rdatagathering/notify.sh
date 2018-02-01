7e9029622133c74697dc5bb55bf939d25bdf6442
richardjtelford <richard.telford@bio.uib.no>
2018-02-01 21:05:13 +0100

    loop syntax

--- traits/Rdatagathering/notify.sh
+++ traits/Rdatagathering/notify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/pi/peru/PFT3_Peru/traits/Rdatagathering
+cd ~peru/PFT3_Peru/traits/Rdatagathering
 while inotifywait -e create /home/pi/Desktop/ > new.txt; do
   Rscript check_image.R
-  fi
+  done
