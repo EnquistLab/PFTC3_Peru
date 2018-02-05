669296dfde3d879e7deb10a45a22767ed67c1e00
richardjtelford <richard.telford@bio.uib.no>
2018-02-05 20:02:54 +0000

    working bash script

--- traits/Rdatagathering/notify.sh
+++ traits/Rdatagathering/notify.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ~/peru/PFTC3_Peru/traits/Rdatagathering/
 
-while inotifywait -e create /home/pi/Desktop/ do
-Rscript run_check_image.R
-done  
+while true
+do
+  inotifywait -e create /home/pi/Desktop/ |  Rscript run_check_image.R
+done 
