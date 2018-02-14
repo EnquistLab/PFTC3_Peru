ddb4dd077d1fe2b3a3a5c913950969087eec27f0
audhalbritter <audhalbritter.com>
2018-02-14 20:52:19 +0100

    change location of data fix species names: split authority check species against plant list

--- traits/Ranalysis/DataImport.R
+++ traits/Ranalysis/DataImport.R
@@ -3,12 +3,13 @@
 library("readxl")
 library("tidyverse")
 library("lubridate")
+library("tpl")
 
 #**********************************************************************************
 #### ALL_PRODUCTIVITY
 
 ### VEGETATION
-vegetation <- read_excel("All_productivity.xlsx", sheet = "Vegetación")
+vegetation <- read_excel("traits/data/immasdata/All_productivity.xlsx", sheet = "Vegetación")
 
 vegetation <- vegetation %>%
   rename(Valley = VALLEY, Site = SITE, Year = YEAR, Month = MONTH, Code = CODE, Fire = FIRE, Fence = TREAT, Plot = PLOT, Column = COLUMN, Quadrant = QUADRANT, Genus = GENDER, Circumference1 = CIRCMF1, Circumference2 = CIRCMF2, Height1 = HEIGHT1, Height2 = HEIGHT2, Diameter1 = DIAMETER1, Diameter2 = DIAMETER2) %>% 
@@ -22,7 +23,7 @@ ggplot(vegetation, aes(x = Fire, y = Diameter1)) +
 
 
 ### ABOVEGROUND BIOMASS
-abovegrbiomass <- read_excel("All_productivity.xlsx", sheet = "Pesos Above Ground")
+abovegrbiomass <- read_excel("traits/data/immasdata/All_productivity.xlsx", sheet = "Pesos Above Ground")
 
 abovegrbiomass <- abovegrbiomass %>%
   select(1:(ncol(.)-1)) %>% # remove empty column at end
@@ -32,7 +33,7 @@ abovegrbiomass <- abovegrbiomass %>%
 
 
 ### DEVELOPMENT
-development <- read_excel("All_productivity.xlsx", sheet = "Desarrollo ESP.")
+development <- read_excel("traits/data/immasdata/All_productivity.xlsx", sheet = "Desarrollo ESP.")
 
 development <- development[,1:12] # remove empty columns at end
 
@@ -43,7 +44,7 @@ development <- development %>%
 
 
 ### BELOWGROUND BIOMASS
-belowgrbiomass <- read_excel("All_productivity.xlsx", sheet = "Pesos Below Ground")
+belowgrbiomass <- read_excel("traits/data/immasdata/All_productivity.xlsx", sheet = "Pesos Below Ground")
 
 belowgrbiomass <- belowgrbiomass %>%
   rename(Valley = VALLEY, Site = SITE, Year = YEAR, Month = MONTH, Code = CODE, Fire = FIRE, Fence = TREAT, Plot = PLOT, Column = COLUMN, Cylinder = CYLINDER, Time = TIME, SoilOrganic = `SOIL ORG`, SoilInorganic = `SOIL INORG`) %>% 
@@ -52,14 +53,14 @@ belowgrbiomass <- belowgrbiomass %>%
 
 
 ### CLIMATE
-climate <- read_excel("All_productivity.xlsx", sheet = "Humedad-Temperatura")
+climate <- read_excel("traits/data/All_productivity.xlsx", sheet = "Humedad-Temperatura")
 
 # MISSING COLUMN NAMES!!!
 
 
 
 ### RESPIRATION
-respiration <- read_excel("All_productivity.xlsx", sheet = "Respiracion")
+respiration <- read_excel("traits/data/immasdata/All_productivity.xlsx", sheet = "Respiracion")
 
 respiration <- respiration %>%
   rename(Valley = VALLEY, Site = SITE, Year = YEAR, Code = CODE, Fire = FIRE, Fence = TREAT, Plot = PLOT, Column = COLUMN, Cylinder = CYLINDER, Plot2 = `;Plot`, CO2Ref = `CO2 Ref`, mbRef = `mb Ref`, mbRTemp = `mbR Temp`, InputA = `Input A`, InputB = `Input B`, InputC = `Input C`, InputD = `Input D`, InputE = `Input E`, InputF = `Input F`, InputG = `Input G`, InputH = `Input H`) %>% 
@@ -69,15 +70,26 @@ respiration <- respiration %>%
 
 #**********************************************************************************
 ### SPECIES
-sp <- read_excel("Especies Puna.xlsx")
+sp <- read_excel(path = "traits/data/immasdata/Especies Puna.xlsx")
 
-speceis <- sp %>% 
+species <- sp %>% 
   select(Copdigo, Especie) %>% 
-  rename(Code = Copdigo, Species = Especie)
+  rename(Code.Imma = Copdigo, SpeciesName = Especie)
+
+taxonomy <- plyr::ldply(strsplit(species$SpeciesName, " "), function(x){
+    data_frame(speciesName = paste(x[1:2], collapse = " "))
+}) %>% 
+  mutate(speciesName = gsub("\\(Less.\\)", "spp", speciesName))
+
+# Check names with species list, tpl
+tpl.get(taxonomy$speciesName)
+# problem species:
+# Calamagrostis rupestris was misspelled|replaced synonym Calamagrostis longearistata
+# Gaultheria myrsinoides                replaced synonym         Pernettya prostrata
 
 
 ### SPECIES COMPOSITION
-abundance <- read_excel("Species_comp.xlsx", sheet = "Species_comp")
+abundance <- read_excel("traits/data/immasdata/Species_comp.xlsx", sheet = "Species_comp")
 # fix year!!!
 
 
@@ -85,7 +97,7 @@ abundance <- read_excel("Species_comp.xlsx", sheet = "Species_comp")
 #### ALL_MATRIX
 
 ### ABOVEGROUND BIOMASS
-agbiomass <- read_excel("All_matrix_2017-11-07.xlsx", sheet = "AGB_matrix")
+agbiomass <- read_excel("traits/data/immasdata/All_matrix_2017-11-07.xlsx", sheet = "AGB_matrix")
 
 agbiomass <- agbiomass %>% 
   rename(Site = SITE, Fire = FIRE, AGbiomass_Mg_ha = AGB.Tnha, AGbiomass_Mg_ha_month = AGB.Tnham) %>% 
@@ -93,7 +105,7 @@ agbiomass <- agbiomass %>%
   mutate(Fence = plyr::mapvalues(Fence, c("Uf", "F"), c("Unfenced", "Fenced")))
 
 ### BELOWGROUND BIOMASS
-bgbiomass <- read_excel("All_matrix_2017-11-07.xlsx", sheet = "BGB_matrix")
+bgbiomass <- read_excel("traits/data/immasdata/All_matrix_2017-11-07.xlsx", sheet = "BGB_matrix")
 
 bgbiomass <- bgbiomass %>% 
   rename(Site = SITE, Fire = FIRE, BGbiomass_Mg_ha = BGB.Tnha, BGbiomass_Mg_ha_month = BGB.Tnha.m) %>% 
@@ -102,7 +114,7 @@ bgbiomass <- bgbiomass %>%
 
 
 ### RESPIRATION
-respiration <- read_excel("All_matrix_2017-11-07.xlsx", sheet = "Resp_matrix")
+respiration <- read_excel("traits/data/immasdata/All_matrix_2017-11-07.xlsx", sheet = "Resp_matrix")
 
 respiration <- respiration %>% 
   rename(Site = SITE, Fire = FIRE, Respiration_MC_C_ha = Rs.Tnha) %>% 
@@ -112,7 +124,7 @@ respiration <- respiration %>%
 
 
 ### NPP
-npp <- read_excel("All_matrix_2017-11-07.xlsx", sheet = "NPP_matrix")
+npp <- read_excel("traits/data/immasdata/All_matrix_2017-11-07.xlsx", sheet = "NPP_matrix")
 
 npp <- npp %>% 
   rename(AGP_Tn_ha = AGP, BGP_Tn_ha = BGP, NPP_Tn_ha = NPP) %>% 
