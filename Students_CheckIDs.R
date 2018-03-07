1a87e73391030ba192c03b035da42ab09b9187d4
audhalbritter <audhalbritter.com>
2018-03-07 16:33:11 -0500

    fix location

--- Students_CheckIDs.R
+++ Students_CheckIDs.R
@@ -2,11 +2,11 @@
 #### CODE TO CHECK SPREADSHEETS ####
 ####################################
 
-# Source to load libraries, data lists and functions
+#### Source to load libraries, data lists and functions ####
 source("traits/Rdatagathering/CheckSpreadsheet.R")
 
 
-# Read in spreadsheet
+#### Read in spreadsheet ####
 ### CHANGE PATH TO THE SPREADSHEET !!!
 traits <- read_excel(path = "traits/data/TraitSpreadsheet_Template.xlsx")
 
@@ -17,3 +17,4 @@ CheckSpreadsheet(dat = traits)
 
 # Draw some plots
 MakeSomePlots(traits)
+
