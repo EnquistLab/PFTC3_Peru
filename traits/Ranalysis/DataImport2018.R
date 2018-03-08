6027a3acad4a4f7b8dd9a493e65fb8b3cbdf7835
audhalbritter <audhalbritter.com>
2018-03-08 15:13:02 -0500

    correct species names

--- traits/Ranalysis/DataImport2018.R
+++ traits/Ranalysis/DataImport2018.R
@@ -34,14 +34,18 @@ coords <- coordinates_2018 %>%
 # Read in species list and check names
 species <- read_excel("community/data/2018-03-08-Species_List_2018.xlsx")
 
-species <- species %>% 
-  mutate(Species = gsub(" ", "_", Species))
-  
 # Check names with species list, tpl
 sp.check <- tpl.get(species$Species)
 
 sp.check %>% 
-  select(name, note, original.search)
+  select(name, note, original.search) %>% 
+  filter(note %in% c("was misspelled"))
+
+species <- species %>% 
+  mutate(Species = gsub(" ", "_", Species)) %>% 
+  # remove duplicates
+  distinct(Species, Family, FunctionalGroup) %>% 
+  rename(species = Species, family = Family,  functionalGroup = FunctionalGroup)
 
 
 
@@ -49,30 +53,41 @@ sp.check %>%
 cover <- read_excel("community/data/2018-03-07_Peru.cover.data.xlsx", sheet = "Cover", col_types = c("text", "text", "text", "numeric", "text", "numeric", "text", "text", "text", "text"))
 
 cover <- cover %>% 
+  rename(successionalStage = treatment) %>% 
   mutate(site = plyr::mapvalues(site, c("WAY", "ACJ", "PIL", "TRE", "QUE"), c("WAY_3100m", "ACJ_3400m", "PIL_3600m", "TRE_3700m", "QUE_3900m"))) %>% 
   mutate(site = factor(site, level = c("WAY_3100m", "ACJ_3400m", "PIL_3600m", "TRE_3700m", "QUE_3900m"))) %>% 
-  mutate(treatment = plyr::mapvalues(treatment, c("C", "B", "BB"), c("Late", "Early", "Very Early"))) %>%
-  mutate(treatment = factor(treatment, level = c("Late", "Early", "Very Early"))) %>% 
-  left_join(species, by = c("species" = "Species"))
+  mutate(successionalStage = plyr::mapvalues(successionalStage, c("C", "B", "BB"), c("Late", "Early", "Recent"))) %>%
+  mutate(successionalStage = factor(successionalStage, level = c("Late", "Early", "Recent"))) %>% 
+  left_join(species, by = "species")
   
 
+### Correct misspellings ####
+# according to TNRS: Pseudognaphalium ramosissimum <- Gnaphalium ramosissimum
+
+cover <- cover %>% 
+  left_join(sp.correct, by = c("species" = "old.sp")) %>% 
+  mutate(species = gsub("Acaena_cylindrystachya", "Acaena_cylindristachya", species),
+         species = gsub("Paspallum_bonplandianum", "Paspalum_bonplandianum", species),
+         species = gsub("Rhynchosphora_macrochaeta", "Rhynchospora_macrochaeta", species),
+         species = gsub("Viola_pygmea", "Viola_pygmaea", species),
+         species = gsub("Melpomene_miniliformis", "Melpomene_moniliformis", species))
 
 
 # Calculate highest cover
 cover.max <- cover %>% 
-  group_by(site, species, treatment) %>% 
+  group_by(site, species, successionalStage) %>% 
   summarise(n = n(), mean = mean(cover, na.rm = TRUE), se = sd(cover, na.rm = TRUE)/sqrt(n)) %>% 
-  arrange(site, treatment, -mean, se)
+  arrange(site, successionalStage, -mean, se)
 
 write_csv(cover.max, path = "community/data/scover.max.csv")
 
 
-### Cover in each site and by treatment
+### Cover in each site and by successionalStage
 cover %>% 
-  group_by(site, species, treatment) %>% 
+  group_by(site, species, successionalStage) %>% 
   summarise(n = n(), mean = mean(cover, na.rm = TRUE), se = sd(cover, na.rm = TRUE)/sqrt(n)) %>% 
   filter(!is.na(mean)) %>% 
-  filter(site == "WAY_3100m", treatment == "Control") %>% 
+  filter(site == "WAY_3100m", successionalStage == "Early") %>% 
   ggplot(aes(x = species, y = mean, ymin = mean - se, ymax = mean + se, color = species)) +
   geom_point() +
   geom_errorbar() +
@@ -84,14 +99,14 @@ cover %>%
 
 # Cover by functional Group
 coverPlot <- cover %>% 
-  group_by(site, treatment, FunctionalGroup) %>% 
+  group_by(site, successionalStage, functionalGroup) %>% 
   summarise(n = n(), mean = mean(cover, na.rm = TRUE), se = sd(cover, na.rm = TRUE)/sqrt(n)) %>%   
-  filter(!is.na(FunctionalGroup), treatment != "Double Burnt") %>% 
-  ggplot(aes(x = FunctionalGroup, y = mean, ymin = mean - se, ymax = mean + se, color = FunctionalGroup)) +
+  filter(!is.na(functionalGroup), successionalStage != "Recentt") %>% 
+  ggplot(aes(x = functionalGroup, y = mean, ymin = mean - se, ymax = mean + se, color = functionalGroup)) +
   geom_point(size = 3) +
   geom_errorbar() +
   labs(x = "", y = "Mean cover in %") +
-  facet_grid(treatment ~ site) +
+  facet_grid(successionalStage ~ site) +
   theme_bw() +
   theme(legend.position = "none", 
         axis.text.x = element_text(angle = 45, hjust = 1),
@@ -103,20 +118,20 @@ ggsave(coverPlot, filename = "cover.jpeg", dpi = 300)
 
 # Species richness
 total <- cover %>% 
-  group_by(site, treatment) %>% 
+  group_by(site, successionalStage) %>% 
   distinct(species) %>% 
   summarise(total = n())
 
 richnessPlot <- cover %>% 
-  filter(!is.na(FunctionalGroup)) %>% 
-  left_join(total, by = c("site", "treatment")) %>%
-  group_by(site, treatment, FunctionalGroup) %>% 
+  filter(!is.na(functionalGroup)) %>% 
+  group_by(site, successionalStage, functionalGroup) %>% 
   summarise(count = n_distinct(species)) %>% 
-  ggplot(aes(x = FunctionalGroup, y = count, fill = FunctionalGroup)) +
+  left_join(total, by = c("site", "successionalStage")) %>%
+  ggplot(aes(x = functionalGroup, y = count, fill = functionalGroup)) +
   geom_bar(stat="identity") +
   labs(x = "", y = "Species richness (count)") +
-  geom_text(aes(x = 5, y = 12, label = "total = ")) +
-  facet_grid(treatment ~ site) +
+  geom_text(aes(x = 4, y = 15, label = paste("total = ", total))) +
+  facet_grid(successionalStage ~ site) +
   theme_bw() +
   theme(legend.position = "none", 
         axis.text.x = element_text(angle = 45, hjust = 1),
@@ -126,6 +141,7 @@ richnessPlot <- cover %>%
 ggsave(richnessPlot, filename = "richness.jpeg", dpi = 300)
 
 unique(cover$species)
+cover %>% group_by(functionalGroup) %>% summarise(count = n_distinct(species))
 
 
 # Get distinct species list
