13d13d4fd0922815f07173ccbb9c2d23689ce441
audhalbritter <audhalbritter.com>
2018-09-04 17:03:41 +0200

    add ID, and nr of leaves per sp x site

--- traits/Rdatagathering/exportLeaves.R
+++ traits/Rdatagathering/exportLeaves.R
@@ -2,12 +2,14 @@ source("DataImport2018.R")
 
 load(file = "traits.cover2.Rdata")
 Export_Species <- traits.cover %>% 
-  select(Site, Elevation, family, Genus, Species, Experiment, Plot, Individual_nr, ID) %>% 
+  select(Site, Elevation, family, Genus, Species, Experiment, Plot, Individual_nr, ID, Nr_leaves) %>% 
+  mutate(Nr_leaves = ifelse(is.na(Nr_leaves), 1, Nr_leaves)) %>% 
   left_join(coords, by = "Site") %>% 
   select(-Elev) %>% 
   group_by(Site, Elevation, Lat, Long, family, Genus, Species) %>% 
-  mutate(Number_Plants = n()) %>% 
-  arrange(Site, Elevation, family, Genus, Experiment, Plot)
+  summarize(Number_Plants = n(), AllIDs = paste(ID, collapse = ", "), Nr_Leaf = paste(Nr_leaves, collapse = ", ")) %>% 
+  arrange(Site, Elevation, family, Genus)
+  
 
 writexl::write_xlsx(x = Export_Species, path = "ExportSpeciesList.xlsx")
 
