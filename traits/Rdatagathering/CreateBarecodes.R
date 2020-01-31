b8b19092acbc3196c8137a8c9d4fd6ceff47390a
audhalbritter <audhalbritter.com>
2020-01-31 13:31:03 +0100

    Magic numbers to fit the labels (only took 50 trials)

--- traits/Rdatagathering/CreateBarecodes.R
+++ traits/Rdatagathering/CreateBarecodes.R
@@ -22,10 +22,12 @@ custom_create_PDF(user = FALSE, Labels = unusedIDs, name = "traits/Peru2020_myla
 unusedIDs2 <- unusedIDs[1:48,]
 
 # Function to make and print labels on PDF
+# The magic combination for these lables: Avery 4778
+#https://www.lyreco.com/webshop/NONO/etiketter-avery-45-7-x-21-2-mm-hvit-eske-c3a0-960-stk-product-000000000002760191.html
 custom_create_PDF(Labels = unusedIDs2, name = "traits/LabelsOut",
                   type = "linear", Fsz = 14, Across = TRUE,
                   trunc = TRUE, numrow = 12, numcol = 4,
-                  page_width = 8.3, page_height = 11.7, width_margin = 0,
-                  height_margin = 0.3, label_width = 1.811, label_height = 0.804)
+                  page_width = 8.3, page_height = 11.7, width_margin = 0.2,
+                  height_margin = 0.7, label_width = 1.811, label_height = 0.5)
 
 
