1b7dd0dcada0d3e653c6976cd5ec50a1d1938728
audhalbritter <audhalbritter.com>
2020-02-11 23:04:27 +0100

    add new Puna comm and trait data

--- traits/Rdatagathering/Merge_PFCT_PU_Data.R
+++ traits/Rdatagathering/Merge_PFCT_PU_Data.R
@@ -24,7 +24,7 @@ get_file(node = "7mzjk",
 
 
 # Load community files
-commPU.raw <- read_excel(path = "community/PU.1_Community_Dataset.xlsx")
+commPU.raw <- read_excel(path = "community/PU.1_Community_Dataset_2018_2019.xlsx")
 commPFTC.raw <- read_csv(file = "community/PFTC3.1_CommunityCover_2018_Peru.csv")
 
 # load data dictioniary
@@ -34,22 +34,20 @@ dicitionary <- read_excel(path = "community/Real_sp_names.xlsx") %>%
 
 # download and read in coords
 drive_download("PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx", path = "traits/data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx")
-coordinates_Peru_2020 <- read_excel("traits/data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx")
+coordinates_Peru_2020 <- read_excel("traits/data/PU.10_PFTC3.10_2020_Peru_Coordinates.xlsx") %>% 
+  mutate(PlotID = as.character(PlotID))
 
 commPU.raw %>% 
-  mutate(PlotID = as.numeric(substr(plot, 2, 2)),
-         Treatment = substr(plot, 1, 1)) %>% 
-  mutate(site = recode(site, "PILL" = "PIL")) %>% 
-  rename("Site" = "site", "Year" = "year", "Taxon" = "name", "FunctionalGroup" = "habit") %>% 
-  select(-plot, -"...14", -"...15", -"...16", -"...17") %>% 
-  distinct() %>% # one duplicate row is removed 
-  left_join(coordinates_Peru_2020, by = c("Site", "Treatment", "PlotID")) %>% 
-  anti_join(dicitionary, by = c("Taxon" = "name", "real_species_name" = "real_species _name"))
-  ### PROBLEM 860 species do not match!!!
+  rename("PlotID" = "plot", "Treatment" = "treatment", "Site" = "site", "Year" = "year", "Taxon" = "name", "FunctionalGroup" = "functional_group") %>% 
+  distinct() %>% # 2 duplicate rows removed 
+  mutate(Campaign = "Puna") %>% 
+  select(-latitude, -longitude, -elevation) %>% 
+  left_join(coordinates_Peru_2020, by = c("Site", "Treatment", "PlotID"))
+  #anti_join(dicitionary, by = c("Taxon" = "name", "real_species_name" = "real_species _name"))
   # left_join(dicitionary, by = c("Taxon" = "name", "real_species_name" = "real_species _name")) %>% 
-  # mutate(Campaign = "Puna") %>% 
   
 commPFTC.raw %>% 
+  left_join(coordinates_Peru_2020, by = c("Site", "Treatment", "PlotID")) %>% 
   anti_join(dicitionary, by = c("Taxon" = "name", "Family"))
   ### PROBLEM 239 species do not match!!!
   #left_join(dicitionary, by = c("Taxon" = "name", "Family)) %>% 
@@ -68,36 +66,32 @@ get_file(node = "7mzjk",
          remote_path = "Peru")
 
 # Load files to R
-traitsPU.raw <- read_excel(path = "traits/data/PU.7_Traits_Dataset.xlsx")
+traitsPU.raw <- read_excel(path = "traits/data/PU.7_Traits_Dataset_2019.xlsx")
 traitsPFTC.raw <- read_csv(file = "traits/data/PFTC3.7_Traits_2018_Peru_cleaned.csv")
 
 
 traitsPU.raw %>% 
-  rename("Leaf_Area_cm2" = "leaf_area", "Dry_Mass_g" = "Dry_mass", "comment" = "commet") %>% 
-  mutate(Leaf_Area_cm2 = as.numeric(if_else(Leaf_Area_cm2 == "sin scan", NA_character_, Leaf_Area_cm2))) %>% 
+  select(-latitude,  -...10, -elevation) %>% 
+  rename("Year" = "year", "Project" = "project", "PlotID" = "plot_id", "Treatment" = "treatment", "Site" = "site", "Taxon" = "taxon", "Plant_Height_cm" = "plant_height_cm", "Individual_nr" = "individual_nr", "NrLeaves" = "nr_leaves", "Bulk" = "bulk", "Leaf_Area_Total_cm2" = "leaf_area.y", "Wet_Mass_Total_g" = "wet_mass_total_g", "Dry_Mass_Total_g" = "dry_mass", "Leaf_Thickness_1_mm" = "leaf_thickness_1_mm", "Leaf_Thickness_2_mm" = "leaf_thickness_2_mm", "Leaf_Thickness_3_mm" = "leaf_thickness_3_mm", "comment" = "commet") %>% 
+  mutate(Leaf_Area_Total_cm2 = as.numeric(if_else(Leaf_Area_Total_cm2 == "whithouth_scan", NA_character_, Leaf_Area_Total_cm2))) %>% 
   left_join(coordinates_Peru_2020, by = c("Site", "Treatment", "PlotID")) %>% 
-  # remove underscore in sp name
-  mutate(Taxon = str_replace(Taxon, "_", " "),
-         Country = "PE",
-         Campaign = "Puna") %>% 
-  
+  mutate(Country = "PE",
+         Campaign = "Puna")
+
   ### CALCULATE LEAF FUNCTIONAL TRAITS
   # SLA, LDMC
   # Species with leaf number = > Leaf nr = 1 because of calculations below
-  mutate(NrLeaves = ifelse(Genus %in% c("Baccharis", "Lycopodiella", "Lycopodium"), 1, NrLeaves)) %>%
+  #mutate(NrLeaves = ifelse(Genus %in% c("Baccharis", "Lycopodiella", "Lycopodium"), 1, NrLeaves)) %>%
   # Bulk samples contain several leaves. We want mass and area on the leaf level
-  rename(Wet_Mass_Total_g = Wet_Mass_g,
-         Dry_Mass_Total_g = Dry_Mass_g,
-         Leaf_Area_Total_cm2 = Leaf_Area_cm2) %>% 
   mutate(Wet_Mass_g = Wet_Mass_Total_g / NrLeaves,
          Dry_Mass_g = Dry_Mass_Total_g / NrLeaves,
          Leaf_Area_cm2 = Leaf_Area_Total_cm2 / NrLeaves) %>%
   # Special cases
   # Sisyrinchium: leaves are folded: area needs to be doubled and leaf thickness halfed
-  mutate(Leaf_Area_cm2 = ifelse(Genus == "Sisyrinchium", Leaf_Area_cm2 * 2, Leaf_Area_cm2),
-         Leaf_Thickness_1_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_1_mm / 2, Leaf_Thickness_1_mm),
-         Leaf_Thickness_2_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_2_mm / 2, Leaf_Thickness_2_mm),
-         Leaf_Thickness_3_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_3_mm / 2, Leaf_Thickness_3_mm)) %>% 
+  # mutate(Leaf_Area_cm2 = ifelse(Genus == "Sisyrinchium", Leaf_Area_cm2 * 2, Leaf_Area_cm2),
+  #        Leaf_Thickness_1_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_1_mm / 2, Leaf_Thickness_1_mm),
+  #        Leaf_Thickness_2_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_2_mm / 2, Leaf_Thickness_2_mm),
+  #        Leaf_Thickness_3_mm = ifelse(Genus == "Sisyrinchium", Leaf_Thickness_3_mm / 2, Leaf_Thickness_3_mm)) %>% 
   # Leaf thickness
   mutate(Leaf_Thickness_Ave_mm = rowMeans(select(., matches("Leaf_Thickness_\\d_mm")), na.rm = TRUE))
 
