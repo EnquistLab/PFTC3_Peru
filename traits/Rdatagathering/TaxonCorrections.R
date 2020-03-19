a79cb6ecdd57fa7cfa99dec7ef694dc5c9df012f
audhalbritter <audhalbritter.com>
2020-03-19 18:04:09 +0100

    initial commit

---
+++ traits/Rdatagathering/TaxonCorrections.R
@@ -0,0 +1,47 @@
+# Taxon Library
+
+GenusDictionary2020 <- read.table(header = TRUE, 
+                                  stringsAsFactors = FALSE, 
+                                  text = "wrong right
+                                  Anatherastipa Anatherostipa
+                                  Antenneria Antennaria
+                                  Belonathus Belonanthus
+                                  Caramagrostis Calamagrostis
+                                  COrtaderia Cortaderia
+                                  Helenia Halenia
+                                  PAspalum Paspalum
+                                  rhynchospora Rhynchospora
+                                  Rhyncospora Rhynchospora
+                                  Rynchospora Rhynchospora
+                                  Tricophorum Trichophorum"
+)
+
+
+
+SpeciesDictionary2020 <- read.table(header = TRUE, 
+                                    stringsAsFactors = FALSE, 
+                                    text = "wrong right
+                                    tricoides trichodes
+                                    'hans meyeni' hans-meyeri
+                                    hans-meyen hans-meyeri
+                                    nigida rigida
+                                    boliniensis boliviensis
+                                    bolivuensis boliviensis
+                                    pinchinchensis pichinchensis
+                                    pigmea pygmaea
+                                    pigmeo pygmaea
+                                    cheilantoides cheilanthoides
+                                    chelantoides cheilanthoides
+                                    ennestii ernestii
+                                    tanaxacoides taraxacoides
+                                    arbiculata orbiculata
+                                    racem racemosa
+                                    racemoss racemosa
+                                    vacenosa racemosa
+                                    claratum clavatum
+                                    hieracoides hieracioides
+                                    ore oreocharis
+                                    pugens pungens
+                                    postrata prostrata"
+)
+
